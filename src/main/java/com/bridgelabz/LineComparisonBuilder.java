4851d1d5214b9551ae15fd0ab3224086b719fe46
anupama cv <anu.anupamacv@gmail.com>
2021-05-20 01:26:05 +0530

    ADDED: welcome messege

---
+++ src/main/java/com/bridgelabz/LineComparisonBuilder.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComparisonBuilder {
+    public static void main(String[] arg){
+        System.out.println(" Welcome to line comparison computation program ");
+    }
+}
